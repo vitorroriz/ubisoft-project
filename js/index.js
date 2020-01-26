21dbdacc56ba4899d957de68d4da043f129b748a
Vitor Roriz <v.r.roriz@gmail.com>
2020-01-26 15:56:46 +0100

    Add first vue app and components.
    Creates buttons for each alphabet letter.

--- js/index.js
+++ js/index.js
@@ -1,18 +1,35 @@
 "use strict";
+window.onload = function() {
 
-var foo;
-console.log(typeof(foo));
-foo = "hello";
-console.log(typeof(foo));
-foo = 3;
-console.log(typeof(foo));
-foo = 3.14;
-console.log(typeof(foo));
-foo = [3.14, 3, 17];
-console.log(typeof(foo));
-foo = true;
-console.log(typeof(foo));
-var bar = null;
-console.log("Type of bar is " + typeof bar);
-var arr = [];
-console.log(typeof arr);
+var startButton = new Vue({
+    el: "#start-button",
+    data: {
+        message: "Play!"
+    }, 
+    methods: {
+        update: function() {
+            this.message = "Restart!";
+        }
+    }
+});
+
+Vue.component('letter-button', {
+    props: ['letter'],
+    template: '<button class="letter-button">{{letter.text}}</button>'
+}
+)
+
+var game = new Vue ({
+    el: "#game",
+    data: {
+        letterList: [/*{id:0, text:""}*/]
+    } 
+});
+
+const LETTERS_IN_ALPHABET = 26;
+const FIRST_LETTER_CODE = 65;
+for(var i = 0; i < LETTERS_IN_ALPHABET; ++i) {
+    game.letterList.push({id:i, text:String.fromCharCode(FIRST_LETTER_CODE+i)});
+}
+
+};
